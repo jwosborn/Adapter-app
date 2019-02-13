24ae04305c46c1e3b930f3c0f3454ddeefd18b6c
kelseymegan92 <kelseymegan92@Kelseys-MBP.lan1>
2019-02-13 18:56:33 -0500

    wrote 'Solutionchoice func that will match opts array with op array

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import './App.css';
 class App extends Component {
 
   render() {
+
     return (
       <div className="App">
         <Header />
--- src/Select.js
+++ src/Select.js
@@ -1,45 +1,51 @@
 import React from 'react';
-// import Solution from './Solution';
+import Solution from './Solution';
 import './App.css'; 
 
 
 
 const Select = ({opts, dopts}) => {
+
   const roomValue = (i) => {
     console.log(i.currentTarget.value);
     return(
       i.currentTarget.value
     );}
 
+  const Solutionchoice =() => {
+    if (opts.includes(roomValue)){
+      return <Solution />
+      } 
+    }
 
     return (
       <div className="Choice-parent">
-      <div>
-        <label htmlFor="Room-List" className="label-text">Choose Your Classroom:</label><br />
-        <select className="Room-list" id="Room-list" onChange={roomValue}> 
-          <option defaultValue="" selected>Choose Your Classroom</option>
+        <div>
+          <label htmlFor="Room-List" className="label-text">Choose Your Classroom:</label><br />
+          <select className="Room-list" id="Room-list" onChange={roomValue}> 
+            <option  selected>Choose Your Classroom</option>
+            {
+              opts.map(op => (
+                <option value={op.roomNumber}>
+                  {op.roomNumber}
+                </option>
+              ))
+            }
+          </select>
+        </div>
+        <div>
+          <label htmlFor="Device-List" className="label-text">Choose Your Device:</label><br />
+          <select className="Device-list">
+          <option defaultValue="" selected>Choose Your Device</option> 
           {
-            opts.map(op => (
-              <option value={op.roomNumber}>
-                {op.roomNumber}
-              </option>
-            ))
-          }
-        </select>
+              dopts.map(op => (
+                <option value={op.name}>
+                  {op.name}
+                </option>
+              ))
+            }
+          </select>
       </div>
-      <div>
-        <label htmlFor="Device-List" className="label-text">Choose Your Device:</label><br />
-        <select className="Device-list">
-         <option defaultValue="" selected>Choose Your Device</option> 
-         {
-            dopts.map(op => (
-              <option value={op.name}>
-                {op.name}
-              </option>
-            ))
-          }
-        </select>
-    </div>
     </div>
     );
 }
