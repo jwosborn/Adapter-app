da188fd9580936a19f614c44a71de6b44f81b933
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-02-23 19:09:16 -0500

    router

---
+++ src/API/router.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/buildings', (req, res) => {
+	// 
+})
+
+router.get("/rooms", (req, res) => {
+	// 
+})
+
+
+
+module.exports = router
