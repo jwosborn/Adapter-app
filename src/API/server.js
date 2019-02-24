3f228f6209a39055008e6591b9323cca6c324384
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-02-24 12:54:23 -0500

    route errors and server errors

--- src/API/server.js
+++ src/API/server.js
@@ -10,4 +10,13 @@ app.get("/healthcheck", (req, res) => {
 // Scoping to localhost:4000/api/...
 app.use("/api", api)
 
+// Invalid route
+app.use(function(req, res, next) {
+  return res.status(404).send({ code: 404, message: `Route ${req.url} not available.` });
+})
+// All server errors
+app.use(function(err, req, res, next) {
+  return res.status(500).send({ error: err });
+});
+
 app.listen(port, () => console.log(`App running at http://localhost:${port}`))
