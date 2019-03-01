3d5130000d5a5de5fec7315a4622ab515775bcb1
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-03-01 08:45:04 -0500

    husky config

--- src/API/server.js
+++ src/API/server.js
@@ -5,6 +5,7 @@ const port = 4000
 const api = require('./api.js')
 
 // app.all('/api/*', requireAuthentication);
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000/*')
   res.header(
