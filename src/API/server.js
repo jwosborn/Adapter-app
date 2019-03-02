0bbdfd35d6cc04b8a5a0f201e9767760bf4c29b9
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-03-02 17:58:06 -0500

    update cors

--- src/API/server.js
+++ src/API/server.js
@@ -7,7 +7,7 @@ const api = require('./api.js')
 // app.all('/api/*', requireAuthentication);
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000/*')
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept',
