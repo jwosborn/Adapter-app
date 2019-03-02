af2e341e1f6f3e529c93d7d59ab82174cc56d158
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-03-02 18:51:18 -0500

    remove headers middleware for cors

--- src/API/server.js
+++ src/API/server.js
@@ -8,15 +8,6 @@ const cors = require('cors')
 // app.all('/api/*', requireAuthentication);
 app.use(cors())
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept',
-  )
-  next()
-})
-
 app.get('/', (req, res) => {
   res.redirect('/api')
 })
