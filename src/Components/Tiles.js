6c48f67e1bfbcbf27423161ef66ad63132387d13
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-03-03 10:23:47 -0500

    setDevice scaffold

--- src/Components/Tiles.js
+++ src/Components/Tiles.js
@@ -9,6 +9,8 @@ class Tiles extends Component {
     rooms: [],
     room: '',
     roomData: {},
+    devices: [],
+    device: '',
   }
 
   setBuilding = building => {
@@ -17,6 +19,7 @@ class Tiles extends Component {
       .then(res => {
         this.setState({ building: building, rooms: res.data })
       })
+      .catch(err => console.log(err))
   }
 
   setRoom = room => {
@@ -26,18 +29,30 @@ class Tiles extends Component {
       .then(res => {
         this.setState({ room: room, roomData: res.data[0] })
       })
+      .catch(err => console.log(err))
+  }
+
+  setDevice = device => {
+    this.setState({ device: device })
   }
 
   componentDidMount() {
     axios.get('http://localhost:4000/api/buildings').then(res => {
       this.setState({ buildings: res.data })
     })
+    axios.get('http://localhost:4000/api/devices').then(res => {
+      this.setState({ devices: res.data })
+    })
   }
 
   render() {
-    const { buildings, rooms } = this.state
+    const { buildings, rooms, device, devices } = this.state
     return (
       <div className="tile-wrapper">
+        {device &&
+          devices.map((dev, index) => (
+            <Tile key={index} text={dev.name} func={this.setDevice} />
+          ))}
         {rooms.length === 0 &&
           buildings.map((building, index) => (
             <Tile key={index} text={building} func={this.setBuilding} />
