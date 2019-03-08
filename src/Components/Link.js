3c27a3079d7d5590201de9b58215cd26b913395c
kelseymegan92 <kelseymegan92@Kelseys-MacBook-Pro.local>
2019-03-08 11:54:44 -0500

    reformatted Link to stateless comp

--- src/Components/Link.js
+++ src/Components/Link.js
@@ -1,35 +1,33 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 //  Dynamic Link Population
-class Link extends Component {
-  render() {
-    return (
-      <div>
+const Link = props => {
+  return (
+    <div>
+      <h2>
+        You can buy an {this.props.adapterName1} adapter by clicking{' '}
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href={this.props.adapterLink1}
+        >
+          here
+        </a>
+      </h2>
+      {this.props.twoAdapters ? (
         <h2>
-          You can buy an {this.props.adapterName1} adapter by clicking{' '}
+          You can also buy an {this.props.adapterName2} adapter by clicking{' '}
           <a
             rel="noopener noreferrer"
             target="_blank"
-            href={this.props.adapterLink1}
+            href={this.props.adapterLink2}
           >
             here
           </a>
         </h2>
-        {this.props.twoAdapters ? (
-          <h2>
-            You can also buy an {this.props.adapterName2} adapter by clicking{' '}
-            <a
-              rel="noopener noreferrer"
-              target="_blank"
-              href={this.props.adapterLink2}
-            >
-              here
-            </a>
-          </h2>
-        ) : null}
-      </div>
-    )
-  }
-}
+      ) : null}
+    </div>
+  ) //return
+} //component
 
 export default Link
