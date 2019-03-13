445aa51b357c4904bbdbcf2f75831f82f6e8a04b
kelseymegan92 <kelseymegan92@Kelseys-MacBook-Pro.local>
2019-03-13 14:59:50 -0400

    added styled-components dependency and styled Tile

--- src/Components/Tile.js
+++ src/Components/Tile.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const upperFirst = text => {
   return typeof text === 'string'
@@ -6,10 +7,39 @@ const upperFirst = text => {
     : text
 }
 
+// const tilebounce = keyframes`{
+//   0% {
+//     transform: translateY(15%);
+//   }
+//   50%{
+//     transform: translateY(-10%);
+//   }
+//   100%{
+//     transform: translateY(0);
+//   }
+// }`
+const Box = styled.div`
+  cursor: pointer;
+  position: relative;
+  flex-grow: 1;
+  margin: 2vw 2vh;
+  width: 275px;
+  max-width: 275px;
+  height: 150px;
+  overflow: hidden;
+  border-radius: 8px;
+  box-shadow: -4px 4px 20px 1px black;
+  text-decoration: none;
+  color: inherit;
+  ${'' /* animation: ${tilebounce} 3s;  */}
+  :active {
+    box-shadow: -4px 4px 10px 1px black;
+  }
+`
 const Tile = ({ text, func, id }) => (
-  <div className="tile" onClick={() => func(id)}>
+  <Box onClick={() => func(id)}>
     <h5 className="text">{upperFirst(text)}</h5>
-  </div>
+  </Box>
 )
 
 export default Tile
